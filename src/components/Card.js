83d87940cd951738ae23494c7615c629abde2fde
samergain <samersadon@gmail.com>
2020-11-24 21:15:11 -0500

    created cards and cardsContainer components for projects page and moved the projects info to a json file

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,25 @@
 import React from "react";
 
-function NavTabs() {
+function Card(props) {
   return (
     <>
-
+    <div className="col mb-4">
+      <div className="card">
+          <img src={ props.src } className="card-img-top" alt={ props.alt } />
+          <div className="card-body">
+            <h5 className="card-title">{ props.title }</h5>
+            <a href={ props.repoLink } target="_blank" rel="noreferrer">{ props.title }</a>
+            <p className="card-text">{ props.description}</p>
+          </div>
+      </div>
+    </div>
     </>
   );
 }
 
-export default NavTabs;
+export default Card;
+
+
+                
+
+
--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,11 +1,33 @@
 import React from "react";
+import Card from "./Card";
+import Projects from "../projects.json";
 
-function NavTabs() {
+function CardsContainer() {
   return (
     <>
-
+       <div className="container mt-5 rounded bg-light">
+        <div className="row">
+        <div className="col">
+            <h1 className="pt-3 pl-3">WHAT CAN I DO SO FAR?</h1>
+            <hr />
+            <div className="row row-cols-1 row-cols-md-2">
+                {
+                  Projects.map(project => (
+                    <Card 
+                    src={project.src}
+                    alt ={project.alt}
+                    title= {project.title}
+                    repoLink= {project.repoLink}
+                    description= {project.description}
+                    />
+                  ))
+                }
+            </div>
+        </div>
+    </div>
+    </div>
     </>
   );
 }
 
-export default NavTabs;
+export default CardsContainer;
