468e0aeae351f53a60614bab11fd4f93af4f82a6
samergain <samersadon@gmail.com>
2021-02-04 20:35:10 -0500

    fixing small details in styling for responsive design

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Flip from 'react-reveal/Flip';
-import Jump from 'react-reveal/Jump';
 import './Contact.css';
 
 function Home() {
@@ -8,20 +6,20 @@ function Home() {
     <div className="container mt-5 rounded bg-light">
         <div className="row">
             <div className="col contact">
-                <div class="contact-card">
-                    <i class="fa fa-github-square fa-2x"></i><a href="https://github.com/samergain" target="_blank" rel="noreferrer">https://github.com/samergain</a>
+                <div className="contact-card">
+                    <i className="fa fa-github-square fa-2x"></i><a href="https://github.com/samergain" target="_blank" rel="noreferrer">https://github.com/samergain</a>
                 </div>
-                <div class="contact-card">
-                    <i class="fa fa-linkedin-square fa-2x"></i><a href="https://www.linkedin.com/in/ssamer/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/ssamer/</a>
+                <div className="contact-card">
+                    <i className="fa fa-linkedin-square fa-2x"></i><a href="https://www.linkedin.com/in/ssamer/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/ssamer/</a>
                 </div>
-                <div class="contact-card">
-                    <i class="fa fa-facebook-square fa-2x"></i><a href="https://www.facebook.com/sssamer" target="_blank" rel="noreferrer">https://www.facebook.com/sssamer</a>
+                <div className="contact-card">
+                    <i className="fa fa-facebook-square fa-2x"></i><a href="https://www.facebook.com/sssamer" target="_blank" rel="noreferrer">https://www.facebook.com/sssamer</a>
                 </div>
-                <div class="contact-card">
-                    <i class="fa fa-envelope-square fa-2x"></i><a href="mailto:samersadon@gmail.com" target="_blank" rel="noreferrer">samersadon@gmail.com</a>
+                <div className="contact-card">
+                    <i className="fa fa-envelope-square fa-2x"></i><a href="mailto:samersadon@gmail.com" target="_blank" rel="noreferrer">samersadon@gmail.com</a>
                 </div>
-                <div class="contact-card">
-                    <i class="fa fa-phone-square fa-2x"></i><a href="" target="_blank" rel="noreferrer">+1-202-361-5963</a>
+                <div className="contact-card">
+                    <i className="fa fa-phone-square fa-2x"></i><a href="#" target="_blank" rel="noreferrer">+1-202-361-5963</a>
                 </div>
             </div>
         </div>
